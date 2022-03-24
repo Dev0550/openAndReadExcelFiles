4a7afa06a4a27af39366695236c96825ce88bfa6
Firdavs <Firdavs@DESKTOP-1QSTKRO>
2022-03-25 00:58:42 +0300

    Добавьте файлы проекта.

---
+++ openAndReadExcelFiles/Form1.cs
@@ -0,0 +1,77 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace openAndReadExcelFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook|*.xlsx", Multiselect = false})
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    DataTable dt = new DataTable();
+                    using(XLWorkbook wb = new XLWorkbook(ofd.FileName))
+                    {
+                        bool isFirstRow = true;
+                        var rows = wb.Worksheet(1).RowsUsed();
+                        foreach(var row in rows)
+                        {
+                            if(isFirstRow)
+                            {
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Columns.Add(cell.Value.ToString());
+                                isFirstRow = false;
+                            }
+                            else
+                            {
+                                dt.Rows.Add();
+                                int i = 0;
+                                foreach (IXLCell cell in row.Cells())
+                                    dt.Rows[dt.Rows.Count - 1][i++] = cell.Value.ToString();    
+                            }
+                        }
+                        dgv.DataSource = dt.DefaultView;
+                        lblTotal.Text = $"Количество: {dgv.RowCount}";
+                        Cursor.Current = Cursors.Default;
+                    }
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataView dv = dgv.DataSource as DataView;
+                if (dv != null)
+                    dv.RowFilter = txtSearch.Text;
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+                btnSearch.PerformClick();
+        }
+    }
+}
